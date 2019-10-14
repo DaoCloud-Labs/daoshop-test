d36cad75f76094f0a4e1946a730d238f5b0768d9
Min <minluoying@163.com>
2019-10-14 20:12:42 +0800

    var

--- lib/global_var.py
+++ lib/global_var.py
@@ -1,4 +1,9 @@
-host = "10.15.160.1:30965"
+#!/usr/bin/env python3
+
+import os
+
+host = os.getenv('DS_HOST') if os.getenv('DS_HOST') else "10.15.160.1:30965"
+
 
 url_login = "/demo/user/v1/login"
 url_products = "/demo/product/v1/products"
@@ -9,23 +14,19 @@ auto_username = 'autotest'
 auto_password = 'autopassword'
 no_username = 'user_not_exit'
 wrong_password = '12343244234134'
+mysql_host = '"10.122.22.209"'
+mysql_port = '3306'
+mysql_user = '"root"'
+mysql_pwd = '"Mac_034567"'
+mysql_product_database = '"product"'
+mysql_user_database = '"mac_user_center"'
+mysql_order_database = '"order"'
 
 login_error_msg = "username or password not right!"
 
-ui_registry = "http://10.15.160.1:30965/registry"
-ui_user = "http://10.15.160.1:30965/user"
-ui_login = "http://10.15.160.1:30965/login"
-ui_auto_username = 'autotest'
-ui_auto_password = 'autopassword'
 
-
-
-
-# mysql_host = '"10.122.22.209"'
-# mysql_port = '3306'
-# mysql_user = '"root"'
-# mysql_pwd = '"Mac_034567"'
-# mysql_product_database = '"product"'
-# mysql_user_database = '"mac_user_center"'
-# mysql_order_database = '"order"'
-
+ui_registry = os.getenv('DS_UI_REGISTRY') if os.getenv('DS_UI_REGISTRY') else "http://10.15.160.1:30965/registry"
+ui_user = os.getenv('DS_UI_USER') if os.getenv('DS_UI_USER') else "http://10.15.160.1:30965/user"
+ui_login = os.getenv('DS_UI_LOGIN') if os.getenv('DS_UI_LOGIN') else "http://10.15.160.1:30965/login"
+ui_auto_username = auto_username
+ui_auto_password = auto_password
